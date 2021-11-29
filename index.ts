02e118a6912828e0069125664c10f796c8803261
Kosta Korenkov <kosta@korenkov.net>
2021-11-29 22:56:05 +0300

    feat: ignore punctuation when match titles

--- index.ts
+++ index.ts
@@ -5,9 +5,12 @@ import {
   MovieResultsResponse,
 } from "./types";
 
+const normalize = (str: string) =>
+  str.toLowerCase().replace(/[,.]/g, "").replace(/[-]/g, " ");
+
 const titleMatch = (movieData, needle) =>
-  movieData.title.toLowerCase() === needle.toLowerCase() ||
-  movieData.original_title.toLowerCase() === needle.toLowerCase();
+  normalize(movieData.title) === normalize(needle) ||
+  normalize(movieData.original_title) === normalize(needle);
 
 class TmdbExtractor {
   private tmdbApiKey: string;
@@ -28,14 +31,14 @@ class TmdbExtractor {
   async getExactByTitleAndMaybeYear(
     title: string,
     year?: number,
-    language?: string,
+    language?: string
   ): Promise<number> {
     const results: MovieResultsResponse = await fetch(
       `https://api.themoviedb.org/3/search/movie?api_key=${
         this.tmdbApiKey
-      }&query=${title}&primary_release_year=${year || ""}` + (language ? `&language=${language}` : '')
+      }&query=${title}&primary_release_year=${year || ""}` +
+        (language ? `&language=${language}` : "")
     ).then((res) => res.json() as Promise<MovieResultsResponse>);
-      console.log(results.results);
     const exactMatch = results.results?.find(
       (res) =>
         (res.title?.toLowerCase() === title.toLowerCase() ||
@@ -54,12 +57,18 @@ class TmdbExtractor {
     return exactMatch?.id || 0;
   }
 
-  async getByTitleAndOriginalTitle(title, originalTitle, language): Promise<number> {
+  async getByTitleAndOriginalTitle(
+    title,
+    originalTitle,
+    language
+  ): Promise<number> {
     const byTitle = await fetch(
-      `https://api.themoviedb.org/3/search/movie?api_key=${this.tmdbApiKey}&query=${title}` + (language ? `&language=${language}` : '')
+      `https://api.themoviedb.org/3/search/movie?api_key=${this.tmdbApiKey}&query=${title}` +
+        (language ? `&language=${language}` : "")
     ).then((res) => res.json() as Promise<MovieResultsResponse>);
     const byOriginalTitle = await fetch(
-      `https://api.themoviedb.org/3/search/movie?api_key=${this.tmdbApiKey}&query=${originalTitle}` + (language ? `&language=${language}` : '')
+      `https://api.themoviedb.org/3/search/movie?api_key=${this.tmdbApiKey}&query=${originalTitle}` +
+        (language ? `&language=${language}` : "")
     ).then((res) => res.json() as Promise<MovieResultsResponse>);
     if (!byTitle || !byOriginalTitle) return 0;
     const exactMatch = byTitle.results?.find(
@@ -90,7 +99,11 @@ class TmdbExtractor {
       id = await this.getExactByTitleAndMaybeYear(title, year + 1, language);
     }
     if (!id && originalTitle) {
-      id = await this.getByTitleAndOriginalTitle(title, originalTitle, language);
+      id = await this.getByTitleAndOriginalTitle(
+        title,
+        originalTitle,
+        language
+      );
     }
     if (!id && title) {
       id = await this.getByTitleOnly(title);
